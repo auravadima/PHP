de35f0cb8ec4baa2c9c2eb098fd3cc753841dfde
Kovalev Vadim <auravadima@gmail.com>
2019-10-17 14:26:16 +0700

    now there are random

--- createCsv.py
+++ createCsv.py
@@ -1,5 +1,6 @@
+import random
 with open('numbers.csv', 'w') as file:
   numbers = []
   for i in range(100):
-    numbers.append(str(i + 1))
+    numbers.append(str(random.randint(1, 100)))
   file.write(','.join(numbers))
