672107862b6deaf6a94e2286292f640cf6423270
Vadim Kovalev <auravadima@gmail.com>
2019-10-23 20:11:18 +0700

    move to folders

--- createCsv.py
+++
@@ -1,6 +0,0 @@
-import random
-with open('numbers.csv', 'w') as file:
-  numbers = []
-  for i in range(100):
-    numbers.append(str(random.randint(1, 100)))
-  file.write(','.join(numbers))
--- factorial.py
+++
@@ -1,6 +0,0 @@
-import math
-try:
-  number = int(input("Input number: "))
-  print("Factorial of", number, 'is', math.factorial(number))
-except:
-  print("You must enter a number")
