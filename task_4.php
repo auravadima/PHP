9a9adb79f98f6a9b337fa4e557ef8b1c6d709dd3
Vadim Kovalev <auravadima@gmail.com>
2019-10-30 20:28:03 +0700

    fix incorrect year

--- task_4.php
+++ task_4.php
@@ -1,6 +1,10 @@
 <?php
   $month = 8;
   $year = 2019;
+  if(!is_int($year) || $year < 0 || $year > 10000) {
+    echo "Achtung! Achtung!";
+    exit();
+  }
   for($day = 1; $day <= 31; $day++)
   {
       $date = mktime(0, 0, 0, $month, $day, $year);  
