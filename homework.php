86e6c2a435afc965a8b0dece9a6902bae809068a
Vadim <auravadima@gmail.com>
2019-11-03 15:04:25 +0700

    ready

--- homework.php
+++ homework.php
@@ -4,10 +4,17 @@
   $conn = new PDO("mysql:host=localhost;dbname=my_db;charset=utf8", $username, $password);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  // createRandomUsers($conn);
-  // print_r(getOlderUsers($conn));
-  // print_r(getJsonByPattern($conn));
+  // Задание 1
+  createRandomUsers($conn);
+  // Задание 2
+  print_r(getOlderUsers($conn));
+  // Задание 3
+  print_r(getJsonByPattern($conn));
+  // Задание 4
   print_r(getUpdatedUsers($conn));
+  // Задание 5
+  print_r(getUniqueNames($conn));
+  
 
   function createRandomUsers($conn) {
     $stmt = $conn->prepare("insert into users (name, age) values (:name, :age)");
@@ -26,26 +33,35 @@
   }
 
   function getJsonByPattern($conn) {
-    $stmt = $conn->prepare("select * from users where name like '%av%' or age like '%ab%'");
+    $stmt = $conn->prepare("select * from users where name like '%ав%' or age like '%аб%'");
     $stmt->execute();
-    return json_encode($stmt->fetchAll());
+    return json_encode($stmt->fetchAll(), JSON_UNESCAPED_UNICODE);
   }
 
   function getUpdatedUsers($conn) {
-    $stmt = $conn->prepare(
-      "start transaction; 
-       select * from users where age > 70 for update; 
-       update users SET name = 'Pepito' WHERE age > 70;
-       commit;"
-    );
-    $stmt->execute();
-    return $stmt->fetchAll();
+    $conn->beginTransaction();
+    $select = $conn->prepare("select * from users where age > 70 for update;");
+    $select->execute();
+
+    $update = $conn->prepare("update users SET name = 'Pepito' WHERE age > 70;");
+    $update->execute();
+
+    $conn->commit();
+    return $select->fetchAll();
+  }
+
+  function getUniqueNames($conn) {
+    $select = $conn->prepare("select distinct name from users");
+    $select->execute();
+    return $select->fetchAll();
   }
 
   function generateRandomUser($length = 10) {
+    $chars = "йцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ";
+    $len = 64;
     $randomString = '';
     for ($i = 0; $i < $length; $i++) {
-        $randomString .= chr(rand(97, 122));
+        $randomString .= iconv_substr($chars, rand(0, $len-1), 1, "utf-8");
     }
     return [ucwords($randomString), rand(10, 100)];
   }
