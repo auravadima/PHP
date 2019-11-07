41fde02c644d8f8db794e8c47022f6fb3e4999bb
Kovalev Vadim <auravadima@gmail.com>
2019-11-07 14:27:07 +0700

    Устал

---
+++ homework.php
@@ -0,0 +1,71 @@
+<?php
+
+abstract class Publication {
+    private $title;
+    private $text;
+    private $source;
+
+    public function __construct($json) {
+        $this->title = $json->title;
+        $this->text = $json->text;
+        $this->source = $json->source;
+    }
+
+    public function getSource() {
+        return $this->source;
+    }
+
+    public function getContent() {
+        return $this->text;
+    }
+
+    public abstract function getAll();
+}
+
+class News extends Publication {
+
+    public function getAll() {
+        return $this->title;
+    }
+}
+
+class Announce extends Publication {
+
+    public function getAll() {
+        return $this->title;
+    }
+}
+
+abstract class MysqlConnection {
+    private $conn;
+
+    function __construct() {
+        $this->conn = new PDO("mysql:host=localhost;dbname=my_db", "newuser", "password");
+    }
+
+    function select($columns, $table) {
+        $stmt = $conn->prepare("select $columns from $table");
+        return $stmt->execcute(); 
+    }
+
+    function insert($columns, $table) {
+        $stmt = $conn->prepare("insert $columns from $table");
+        return $stmt->execcute(); 
+    }
+}
+
+class NewsDB extends DB {
+    function all() {
+        $stmt = $conn->prepare("select title, text, link as source from news");
+        return $stmt->execcute();
+    }
+}
+
+class AnnounceDB extends DB {
+    function all() {
+        $stmt = $conn->prepare("select title, text, author as source from announces");
+        return $stmt->execcute();
+    }
+}
+
+class MysqlConnection
