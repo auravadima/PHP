d21d491bc6debb69e1a281b043cd0976880418d3
Vadim <auravadima@gmail.com>
2019-11-14 01:13:10 +0700

    task 4

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,9 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+class TestOne 
+{
+    private $name;
+    private $age;
+    private $male;
+
+    public function getTypes() 
+    {
+        return [
+            'name' => 'string',
+            'age' => 'int',
+            'male' => 'boolean'
+        ];
+    }
+}
+
 class TestController extends Controller
 {
-    public function getOne() {
-        return "OK";
+    public function getOne() 
+    {
+        $testOne = new TestOne();
+        return $testOne->getTypes();
     }
 }
