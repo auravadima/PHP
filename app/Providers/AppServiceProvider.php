19702909234f7c906f3d68069dd9b7c02f195ea8
Vadim <auravadima@gmail.com>
2019-11-24 16:07:34 +0700

    task 7

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,17 +3,18 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Services\TestBasicService;
+use App\TestOne;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
     public function register()
     {
-        //
+        $this->app->bind(TestBasicService::class, function ()   {
+        	return new TestBasicService(new TestOne());
+        });
+        $this->app->alias(TestBasicService::class, 'service.test_one');
+
     }
 
     /**
