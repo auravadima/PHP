a3f6927f87f516bb9975b9198e140dad8f79c96b
Vadim <auravadima@gmail.com>
2019-12-01 22:42:25 +0700

    fix codestyle

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 
-Route::group([ 'prefix' => 'test', 'middleware' => 'api'], function() {
+Route::group(['prefix' => 'test', 'middleware' => 'api'], function () {
     Route::get("one", "TestController@getOne");
     Route::post("one", "TestController@setOne");
 
